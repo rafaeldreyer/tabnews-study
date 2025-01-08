5899bf816de20c21bd6062a770d64632ae628943
Rafael Dreyer <91893501+rafaeldreyer@users.noreply.github.com>
2025-01-08 19:02:18 +0000

    add pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home() {
+    return <h1> Porque, assim como o relâmpago sai 
+                do oriente e se mostra até ao ocidente, <br></br>
+                assim será também a vinda do Filho do homem. Mateus 24:27
+            </h1>
+}
+
+export default Home; // export padrão do NextJs 

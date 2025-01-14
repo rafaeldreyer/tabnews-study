4c36954eb0347505053a2f47fc3dc7bc40b59e36
Rafael Dreyer <91893501+rafaeldreyer@users.noreply.github.com>
2025-01-14 17:48:41 +0000

    add scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabnews-study
+
 Projeto de estudo em programação com base no https://www.tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
-    return <h1> Alguém paga a coca
-            </h1>
+  return <h1> Alguém paga a coca</h1>;
 }
 
-export default Home; // export padrão do NextJs 
+export default Home; // export padrão do NextJs

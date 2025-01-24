b8ff92eda2fb038a3bf199df45899af231195594
Rafael Dreyer <91893501+rafaeldreyer@users.noreply.github.com>
2025-01-24 14:35:59 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-  return <p>This is my "secret lair" of code. Shhh, don't tell anyone. :)</p>;
+  return <p>This is my "secret lair" of code. Shhh, don't tell anyone. :)</p>
+         <a href="https://github.com/rafaeldreyer">Don't click!</a> 
+    ;
 }
 
 export default Home; // export padrão do NextJs
